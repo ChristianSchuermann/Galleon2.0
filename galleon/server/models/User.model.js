23bd279f6cf8cf84e978e7ebbfc624f922a952fc
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-09-09 11:48:31 +0200

    move all server files

--- galleon/server/models/User.model.js
+++ galleon/server/models/User.model.js
@@ -1,20 +1,16 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = mongoose;
+ 
+ 
+const userSchema = new Schema({
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  income: { type: Schema.Types.IncomeID, red: "Income" },
+  expense: { type: Schema.Types.ExpenseID, red: "Expense" },
 
-const User = model("User", userSchema);
 
-module.exports = User;
+});
+ 
+module.exports = model("User", userSchema);
