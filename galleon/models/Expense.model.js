7193926a73d670d57ecd962dd2ee2c4cd70466ca
Christian Schuermann <christian.schuermann.90@gmail.com>
2022-09-14 16:08:52 +0200

    changed folders

---
+++ galleon/models/Expense.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const expenseSchema = new Schema({
+  title: { type: String },
+  description: { type: String },
+  expense: { type: Number },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  category: { type: String },
+});
+
+module.exports = model("Expense", expenseSchema);
+
+/* Client-Side
+-> user
+user.expense --> [.........]
+map ^ => inside */
